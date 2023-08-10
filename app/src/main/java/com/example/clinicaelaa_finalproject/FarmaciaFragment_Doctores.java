779c6d0c93f6c8a9a0cb8d3bcb8f20c74c1c557b
AstridBPR <125079160+AstridBPR@users.noreply.github.com>
2023-08-09 22:37:02 -0600

    Farmacia doctores-usuarios

---
+++ app/src/main/java/com/example/clinicaelaa_finalproject/FarmaciaFragment_Doctores.java
@@ -0,0 +1,14 @@
+package com.example.clinicaelaa_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmaciaFragment_Doctores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmacia_fragment_doctores);
+    }
+}
---
+++ app/src/main/java/com/example/clinicaelaa_finalproject/FarmaciaFragment_Usuarios.java
@@ -0,0 +1,13 @@
+package com.example.clinicaelaa_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmaciaFragment_Usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmacia_fragment_usuarios);
+    }
+}
